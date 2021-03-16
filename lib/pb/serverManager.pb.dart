34fe813340deed4654f6438d94a88dc05569a459
Yu Fang <yu@iotserv.com>
2021-03-16 21:56:15 +0800

    添加Name

--- lib/pb/serverManager.pb.dart
+++ lib/pb/serverManager.pb.dart
@@ -12,16 +12,17 @@ import 'package:protobuf/protobuf.dart' as $pb;
 class ServerInfo extends $pb.GeneratedMessage {
   static final $pb.BuilderInfo _i = $pb.BuilderInfo('ServerInfo', package: const $pb.PackageName('pb'), createEmptyInstance: create)
     ..aOS(1, 'Uuid', protoName: 'Uuid')
-    ..aOS(2, 'ServerHost', protoName: 'ServerHost')
-    ..aOS(3, 'LoginKey', protoName: 'LoginKey')
-    ..aOS(4, 'ConnectionType', protoName: 'ConnectionType')
-    ..a<$core.int>(5, 'TcpPort', $pb.PbFieldType.O3, protoName: 'TcpPort')
-    ..a<$core.int>(6, 'KcpPort', $pb.PbFieldType.O3, protoName: 'KcpPort')
-    ..a<$core.int>(7, 'UdpApiPort', $pb.PbFieldType.O3, protoName: 'UdpApiPort')
-    ..a<$core.int>(8, 'KcpApiPort', $pb.PbFieldType.O3, protoName: 'KcpApiPort')
-    ..a<$core.int>(9, 'TlsPort', $pb.PbFieldType.O3, protoName: 'TlsPort')
-    ..a<$core.int>(10, 'GrpcPort', $pb.PbFieldType.O3, protoName: 'GrpcPort')
-    ..aOS(11, 'Description', protoName: 'Description')
+    ..aOS(2, 'Name', protoName: 'Name')
+    ..aOS(3, 'ServerHost', protoName: 'ServerHost')
+    ..aOS(4, 'LoginKey', protoName: 'LoginKey')
+    ..aOS(5, 'ConnectionType', protoName: 'ConnectionType')
+    ..a<$core.int>(6, 'TcpPort', $pb.PbFieldType.O3, protoName: 'TcpPort')
+    ..a<$core.int>(7, 'KcpPort', $pb.PbFieldType.O3, protoName: 'KcpPort')
+    ..a<$core.int>(8, 'UdpApiPort', $pb.PbFieldType.O3, protoName: 'UdpApiPort')
+    ..a<$core.int>(9, 'KcpApiPort', $pb.PbFieldType.O3, protoName: 'KcpApiPort')
+    ..a<$core.int>(10, 'TlsPort', $pb.PbFieldType.O3, protoName: 'TlsPort')
+    ..a<$core.int>(11, 'GrpcPort', $pb.PbFieldType.O3, protoName: 'GrpcPort')
+    ..aOS(12, 'Description', protoName: 'Description')
     ..hasRequiredFields = false
   ;
 
@@ -50,94 +51,103 @@ class ServerInfo extends $pb.GeneratedMessage {
   void clearUuid() => clearField(1);
 
   @$pb.TagNumber(2)
-  $core.String get serverHost => $_getSZ(1);
+  $core.String get name => $_getSZ(1);
   @$pb.TagNumber(2)
-  set serverHost($core.String v) { $_setString(1, v); }
+  set name($core.String v) { $_setString(1, v); }
   @$pb.TagNumber(2)
-  $core.bool hasServerHost() => $_has(1);
+  $core.bool hasName() => $_has(1);
   @$pb.TagNumber(2)
-  void clearServerHost() => clearField(2);
+  void clearName() => clearField(2);
 
   @$pb.TagNumber(3)
-  $core.String get loginKey => $_getSZ(2);
+  $core.String get serverHost => $_getSZ(2);
   @$pb.TagNumber(3)
-  set loginKey($core.String v) { $_setString(2, v); }
+  set serverHost($core.String v) { $_setString(2, v); }
   @$pb.TagNumber(3)
-  $core.bool hasLoginKey() => $_has(2);
+  $core.bool hasServerHost() => $_has(2);
   @$pb.TagNumber(3)
-  void clearLoginKey() => clearField(3);
+  void clearServerHost() => clearField(3);
 
   @$pb.TagNumber(4)
-  $core.String get connectionType => $_getSZ(3);
+  $core.String get loginKey => $_getSZ(3);
   @$pb.TagNumber(4)
-  set connectionType($core.String v) { $_setString(3, v); }
+  set loginKey($core.String v) { $_setString(3, v); }
   @$pb.TagNumber(4)
-  $core.bool hasConnectionType() => $_has(3);
+  $core.bool hasLoginKey() => $_has(3);
   @$pb.TagNumber(4)
-  void clearConnectionType() => clearField(4);
+  void clearLoginKey() => clearField(4);
 
   @$pb.TagNumber(5)
-  $core.int get tcpPort => $_getIZ(4);
+  $core.String get connectionType => $_getSZ(4);
   @$pb.TagNumber(5)
-  set tcpPort($core.int v) { $_setSignedInt32(4, v); }
+  set connectionType($core.String v) { $_setString(4, v); }
   @$pb.TagNumber(5)
-  $core.bool hasTcpPort() => $_has(4);
+  $core.bool hasConnectionType() => $_has(4);
   @$pb.TagNumber(5)
-  void clearTcpPort() => clearField(5);
+  void clearConnectionType() => clearField(5);
 
   @$pb.TagNumber(6)
-  $core.int get kcpPort => $_getIZ(5);
+  $core.int get tcpPort => $_getIZ(5);
   @$pb.TagNumber(6)
-  set kcpPort($core.int v) { $_setSignedInt32(5, v); }
+  set tcpPort($core.int v) { $_setSignedInt32(5, v); }
   @$pb.TagNumber(6)
-  $core.bool hasKcpPort() => $_has(5);
+  $core.bool hasTcpPort() => $_has(5);
   @$pb.TagNumber(6)
-  void clearKcpPort() => clearField(6);
+  void clearTcpPort() => clearField(6);
 
   @$pb.TagNumber(7)
-  $core.int get udpApiPort => $_getIZ(6);
+  $core.int get kcpPort => $_getIZ(6);
   @$pb.TagNumber(7)
-  set udpApiPort($core.int v) { $_setSignedInt32(6, v); }
+  set kcpPort($core.int v) { $_setSignedInt32(6, v); }
   @$pb.TagNumber(7)
-  $core.bool hasUdpApiPort() => $_has(6);
+  $core.bool hasKcpPort() => $_has(6);
   @$pb.TagNumber(7)
-  void clearUdpApiPort() => clearField(7);
+  void clearKcpPort() => clearField(7);
 
   @$pb.TagNumber(8)
-  $core.int get kcpApiPort => $_getIZ(7);
+  $core.int get udpApiPort => $_getIZ(7);
   @$pb.TagNumber(8)
-  set kcpApiPort($core.int v) { $_setSignedInt32(7, v); }
+  set udpApiPort($core.int v) { $_setSignedInt32(7, v); }
   @$pb.TagNumber(8)
-  $core.bool hasKcpApiPort() => $_has(7);
+  $core.bool hasUdpApiPort() => $_has(7);
   @$pb.TagNumber(8)
-  void clearKcpApiPort() => clearField(8);
+  void clearUdpApiPort() => clearField(8);
 
   @$pb.TagNumber(9)
-  $core.int get tlsPort => $_getIZ(8);
+  $core.int get kcpApiPort => $_getIZ(8);
   @$pb.TagNumber(9)
-  set tlsPort($core.int v) { $_setSignedInt32(8, v); }
+  set kcpApiPort($core.int v) { $_setSignedInt32(8, v); }
   @$pb.TagNumber(9)
-  $core.bool hasTlsPort() => $_has(8);
+  $core.bool hasKcpApiPort() => $_has(8);
   @$pb.TagNumber(9)
-  void clearTlsPort() => clearField(9);
+  void clearKcpApiPort() => clearField(9);
 
   @$pb.TagNumber(10)
-  $core.int get grpcPort => $_getIZ(9);
+  $core.int get tlsPort => $_getIZ(9);
   @$pb.TagNumber(10)
-  set grpcPort($core.int v) { $_setSignedInt32(9, v); }
+  set tlsPort($core.int v) { $_setSignedInt32(9, v); }
   @$pb.TagNumber(10)
-  $core.bool hasGrpcPort() => $_has(9);
+  $core.bool hasTlsPort() => $_has(9);
   @$pb.TagNumber(10)
-  void clearGrpcPort() => clearField(10);
+  void clearTlsPort() => clearField(10);
 
   @$pb.TagNumber(11)
-  $core.String get description => $_getSZ(10);
+  $core.int get grpcPort => $_getIZ(10);
   @$pb.TagNumber(11)
-  set description($core.String v) { $_setString(10, v); }
+  set grpcPort($core.int v) { $_setSignedInt32(10, v); }
   @$pb.TagNumber(11)
-  $core.bool hasDescription() => $_has(10);
+  $core.bool hasGrpcPort() => $_has(10);
   @$pb.TagNumber(11)
-  void clearDescription() => clearField(11);
+  void clearGrpcPort() => clearField(11);
+
+  @$pb.TagNumber(12)
+  $core.String get description => $_getSZ(11);
+  @$pb.TagNumber(12)
+  set description($core.String v) { $_setString(11, v); }
+  @$pb.TagNumber(12)
+  $core.bool hasDescription() => $_has(11);
+  @$pb.TagNumber(12)
+  void clearDescription() => clearField(12);
 }
 
 class ServerInfoList extends $pb.GeneratedMessage {
